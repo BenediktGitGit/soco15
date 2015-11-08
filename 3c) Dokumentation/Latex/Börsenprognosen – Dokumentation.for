853fc534077a96f077ac65faabc248bb07d2772d
Sebascho <sebascho@hotmail.de>
2015-11-08 20:17:20 +0100

    Update

--- 3c) Dokumentation/Latex/Börsenprognosen – Dokumentation.for
+++ 3c) Dokumentation/Latex/Börsenprognosen – Dokumentation.for
@@ -1,10 +1,20 @@
+\defcounter {refsection}{0}\relax 
 \select@language {ngerman}
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{10}
+\defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion}{10}
+\defcounter {refsection}{0}\relax 
 \contentsline {formel}{\numberline {Formel~3.3} Tanh Funktion}{10}
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
