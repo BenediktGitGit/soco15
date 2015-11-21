fb684702aa1acf8e9001d4b9b5caffffd367e156
Sebascho <sebascho@hotmail.de>
2015-11-21 18:56:31 +0100

    Doku Update

--- 3c) Dokumentation/Latex/Börsenprognosen – Dokumentation.for
+++ 3c) Dokumentation/Latex/Börsenprognosen – Dokumentation.for
@@ -2,9 +2,9 @@
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
-\contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{5}
-\contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion}{6}
-\contentsline {formel}{\numberline {Formel~3.3} Tanh Funktion}{6}
+\contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{9}
+\contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion}{11}
+\contentsline {formel}{\numberline {Formel~3.3} Tanh Funktion}{11}
 \addvspace {10\p@ }
 \addvspace {10\p@ }
 \addvspace {10\p@ }
