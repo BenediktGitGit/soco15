7f3a136982f550b9d299ee815631b48ff9193731
Benedikt Hofrichter <bthofrichter@ancud.de>
2015-12-21 22:45:00 +0100

    Umfange Doku

--- 3c_Dokumentation/Masterdokument/Latex/Börsenprognosen – Dokumentation.for
+++ 3c_Dokumentation/Masterdokument/Latex/Börsenprognosen – Dokumentation.for
@@ -7,21 +7,21 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{13}{equation.3.2.1}
+\contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.3.2.1}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.2} Optimale Anzahl Neuronen in der versteckten Schicht}{13}{equation.3.2.2}
+\contentsline {formel}{\numberline {Formel~3.2} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.3.2.2}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.1} Normalisierungsformel}{28}{equation.4.2.1}
+\contentsline {formel}{\numberline {Formel~4.1} Normalisierungsformel}{31}{equation.4.2.1}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.2} Sigmoide Funktion sowie Tanh Funktion}{30}{equation.4.3.2}
+\contentsline {formel}{\numberline {Formel~4.2} Sigmoide Funktion sowie Tanh Funktion}{33}{equation.4.3.2}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{32}{equation.4.3.3}
+\contentsline {formel}{\numberline {Formel~4.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{35}{equation.4.3.3}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{32}{equation.4.3.4}
+\contentsline {formel}{\numberline {Formel~4.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{35}{equation.4.3.4}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.5} MSE zur Berechnung der Abweichung}{33}{equation.4.3.5}
+\contentsline {formel}{\numberline {Formel~4.5} MSE zur Berechnung der Abweichung}{36}{equation.4.3.5}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
