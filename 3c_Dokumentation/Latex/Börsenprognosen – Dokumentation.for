a50baa5161b2bac8fbd07d9dfb88f43c7b393082
Sebascho <sebascho@hotmail.de>
2015-12-21 17:43:18 +0100

    Update Präsentation - BIlder unter Latex -> Bilder

--- 3c_Dokumentation/Latex/Börsenprognosen – Dokumentation.for
+++ 3c_Dokumentation/Latex/Börsenprognosen – Dokumentation.for
@@ -5,25 +5,21 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~2.1} Optimale Anzahl Neuronen in der versteckten Schicht}{15}{equation.2.2.1}
+\contentsline {formel}{\numberline {Formel~2.1} Optimale Anzahl Neuronen in der versteckten Schicht}{13}{equation.2.2.1}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~2.2} Optimale Anzahl Neuronen in der versteckten Schicht}{15}{equation.2.2.2}
-\defcounter {refsection}{0}\relax 
-\addvspace {10\p@ }
-\defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.1} Normalisierungsformel}{19}{equation.3.2.1}
-\defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion sowie Tanh Funktion}{21}{equation.3.3.2}
-\defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{23}{equation.3.3.3}
-\defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{23}{equation.3.3.4}
-\defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.5} MSE zur Berechnung der Abweichung}{24}{equation.3.3.5}
+\contentsline {formel}{\numberline {Formel~2.2} Optimale Anzahl Neuronen in der versteckten Schicht}{13}{equation.2.2.2}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\addvspace {10\p@ }
+\contentsline {formel}{\numberline {Formel~3.1} Normalisierungsformel}{17}{equation.3.2.1}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion sowie Tanh Funktion}{19}{equation.3.3.2}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {Formel~3.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{21}{equation.3.3.3}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {Formel~3.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{21}{equation.3.3.4}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {Formel~3.5} MSE zur Berechnung der Abweichung}{22}{equation.3.3.5}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
