8279ba2cdcb0b88b6aa93d568039fc49d4b7ff87
Benedikt Hofrichter <bthofrichter@ancud.de>
2015-12-21 12:08:14 +0100

    Doku Benedikt V0.1

---
+++ 3c_Dokumentation/Benedikt/Latex/Börsenprognosen – Dokumentation.for
@@ -0,0 +1,13 @@
+\select@language {ngerman}
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\contentsline {formel}{\numberline {Formel~2.1} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.2.5.1}
+\contentsline {formel}{\numberline {Formel~2.2} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.2.5.2}
+\addvspace {10\p@ }
+\contentsline {formel}{\numberline {Formel~3.1} Normalisierungsformel}{28}{equation.3.2.1}
+\contentsline {formel}{\numberline {Formel~3.2} Sigmoide Funktion sowie Tanh Funktion}{30}{equation.3.3.2}
+\contentsline {formel}{\numberline {Formel~3.3} MSE zur Berechnung der Abweichung}{31}{equation.3.3.3}
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
