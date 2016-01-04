a2b20f1c5581e37db2c1cf23e86a0803322b39da
Benedikt Hofrichter <bthofrichter@ancud.de>
2016-01-04 22:54:47 +0100

    Doku v1

--- 3c_Dokumentation/Masterdokument/Latex/Börsenprognosen – Dokumentation.for
+++ 3c_Dokumentation/Masterdokument/Latex/Börsenprognosen – Dokumentation.for
@@ -7,21 +7,23 @@
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.1} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.3.2.1}
+\addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~3.2} Optimale Anzahl Neuronen in der versteckten Schicht}{16}{equation.3.2.2}
+\contentsline {formel}{\numberline {Formel~4.1} Optimale Anzahl Neuronen in der versteckten Schicht}{17}{equation.4.2.1}
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {Formel~4.2} Optimale Anzahl Neuronen in der versteckten Schicht}{17}{equation.4.2.2}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.1} Normalisierungsformel}{31}{equation.4.2.1}
+\contentsline {formel}{\numberline {Formel~5.1} Normalisierungsformel}{32}{equation.5.2.1}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.2} Sigmoide Funktion sowie Tanh Funktion}{33}{equation.4.3.2}
+\contentsline {formel}{\numberline {Formel~5.2} Sigmoide Funktion sowie Tanh Funktion}{34}{equation.5.3.2}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{35}{equation.4.3.3}
+\contentsline {formel}{\numberline {Formel~5.3} Formel zur Bestimmung der Art der Gewichts\IeC {\"a}nderung}{35}{equation.5.3.3}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{35}{equation.4.3.4}
+\contentsline {formel}{\numberline {Formel~5.4} Formel zur Bestimmung des Betrages der Gewichts\IeC {\"a}nderung}{36}{equation.5.3.4}
 \defcounter {refsection}{0}\relax 
-\contentsline {formel}{\numberline {Formel~4.5} MSE zur Berechnung der Abweichung}{36}{equation.4.3.5}
+\contentsline {formel}{\numberline {Formel~5.5} MSE zur Berechnung der Abweichung}{37}{equation.5.3.5}
 \defcounter {refsection}{0}\relax 
 \addvspace {10\p@ }
 \defcounter {refsection}{0}\relax 
